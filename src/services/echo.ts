5cd5c943be1e0578758728df816885b21583dd78
nycolascruz <nycolascruzsilva@gmail.com>
2026-02-20 13:21:40 -0300

    dockerfile

--- src/services/echo.ts
+++ src/services/echo.ts
@@ -7,7 +7,7 @@ export const echo = new Echo({
     broadcaster: "reverb",
     key: "qtsqporbc5kjhir8r9yw",
     wsHost: "localhost",
-    wsPort: 8080,
+    wsPort: 7000,
     forceTLS: false,
     disableStats: true,
 });
